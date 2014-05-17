5660a01c4e718a0f0e57e552f5fddf8b78d095f6
Jesse Hu <huyouare@gmail.com>
2014-05-16 22:40:21 -0400

    Initial Commit

---
+++ PhotoViewer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PhotoViewer
+//
+//  Created by Jesse Hu on 5/16/14.
+//  Copyright (c) 2014 Jesse Hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PhotoViewer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PhotoViewer
+//
+//  Created by Jesse Hu on 5/16/14.
+//  Copyright (c) 2014 Jesse Hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
